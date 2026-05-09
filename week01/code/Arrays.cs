c329e67d682ee9b68290c7e72470a8bd12b93e7d
Rakieft <kieftraphter.joly1@gmail.com>
2026-05-09 11:17:15 -0400

    Arrays is correct

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,33 +1,39 @@
 public static class Arrays
 {
-    /// <summary>
-    /// This function will produce an array of size 'length' starting with 'number' followed by multiples of 'number'.  For 
-    /// example, MultiplesOf(7, 5) will result in: {7, 14, 21, 28, 35}.  Assume that length is a positive
-    /// integer greater than 0.
-    /// </summary>
-    /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Create an array to store the multiples
+        double[] result = new double[length];
 
-        return []; // replace this return statement with your own
+        // Go through each position in the array
+        for (int i = 0; i < length; i++)
+        {
+            // Multiply the number by the position + 1
+            result[i] = number * (i + 1);
+        }
+
+        // Return the completed array
+        return result;
     }
 
-    /// <summary>
-    /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
-    /// List<int>{1, 2, 3, 4, 5, 6, 7, 8, 9} and an amount is 3 then the list after the function runs should be 
-    /// List<int>{7, 8, 9, 1, 2, 3, 4, 5, 6}.  The value of amount will be in the range of 1 to data.Count, inclusive.
-    ///
-    /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
-    /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Find where to split the list
+        int start = data.Count - amount;
+
+        // Save the values that move to the front
+        List<int> endPart = data.GetRange(start, amount);
+
+        // Save the beginning part of the list
+        List<int> firstPart = data.GetRange(0, start);
+
+        // Empty the original list
+        data.Clear();
+
+        // Add the rotated values first
+        data.AddRange(endPart);
+
+        // Add the remaining values
+        data.AddRange(firstPart);
     }
 }
